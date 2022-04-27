0fbe4d1bb06940ea1a4399cc82c0713d5a1336c5
zhenyu_wan888 <zhenyu_wan888@163.com>
2022-04-27 14:13:50 +0800

    update chap3-1

--- container/init.go
+++ container/init.go
@@ -12,7 +12,9 @@ import (
 // 使用mount先去挂载proc文件系统，以便后面通过ps等系统命令去查看当前进程资源的情况
 func RunContainerInitProcess(command string, args []string) error {
 	logrus.Infof("command %s", command)
-
+	// systemd 加入linux之后, mount namespace 就变成 shared by default, 所以你必须显示
+	// 声明你要这个新的mount namespace独立。issue：https://github.com/xianlubird/mydocker/issues/41
+	syscall.Mount("", "/", "", syscall.MS_PRIVATE | syscall.MS_REC, "")
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 	argv := []string{command}
